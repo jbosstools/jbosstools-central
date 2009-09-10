010b7d2b051606867cae819463a39a396a80f6fd
Snjezana Peco <snjezana.peco@redhat.com>
2009-09-10 00:28:19 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4690 Add Maven Portlet library provider
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@17510 a97e2381-89e5-4abb-bab3-167db6db766c

--- maven/plugins/org.jboss.tools.maven.core/src/org/jboss/tools/maven/core/libprov/MavenLibraryProviderInstallOperationConfig.java
+++ maven/plugins/org.jboss.tools.maven.core/src/org/jboss/tools/maven/core/libprov/MavenLibraryProviderInstallOperationConfig.java
@@ -1,9 +1,7 @@
 package org.jboss.tools.maven.core.libprov;
 
-import org.eclipse.emf.ecore.resource.URIConverter.WriteableOutputStream;
 import org.eclipse.jst.common.project.facet.core.libprov.LibraryProviderInstallOperationConfig;
 import org.maven.ide.components.pom.Model;
-import org.maven.ide.components.pom.util.PomResourceImpl;
 
 public class MavenLibraryProviderInstallOperationConfig extends
 		LibraryProviderInstallOperationConfig {
