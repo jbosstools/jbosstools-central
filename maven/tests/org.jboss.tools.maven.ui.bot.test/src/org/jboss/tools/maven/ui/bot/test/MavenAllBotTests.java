0ed942ff9783ce76edf53439f1527f8fd9d93de6
Snjezana Peco <snjezana.peco@redhat.com>
2010-12-28 22:24:18 +0000

    JBIDE-7989 Maven bot test plugin has no runnable tests
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@27784 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ maven/tests/org.jboss.tools.maven.ui.bot.test/src/org/jboss/tools/maven/ui/bot/test/MavenAllBotTests.java
@@ -0,0 +1,16 @@
+package org.jboss.tools.maven.ui.bot.test;
+
+import org.junit.runners.Suite;
+
+/**
+ * 
+ * This is a swtbot testcase for an eclipse application.
+ * 
+ */
+
+@Suite.SuiteClasses({
+	CreateMavenizedSeamProjectTest.class,
+})
+public class MavenAllBotTests {
+
+}
