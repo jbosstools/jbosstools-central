c7e73035b5f9b17c95e6973732028c4a280eb6aa
Rastislav Wagner <rawagner@redhat.com>
2012-02-08 13:15:22 +0000

    removed Display variable, added JAX-RS configurator test
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38495 a97e2381-89e5-4abb-bab3-167db6db766c

--- maven/tests/org.jboss.tools.maven.ui.bot.test/src/org/jboss/tools/maven/ui/bot/test/Configurators.java
+++ maven/tests/org.jboss.tools.maven.ui.bot.test/src/org/jboss/tools/maven/ui/bot/test/Configurators.java
@@ -45,6 +45,7 @@ public class Configurators {
 	public static final String JSF_NATURE="org.jboss.tools.jsf.jsfnature";
 	public static final String JAXRS_NATURE="org.jboss.tools.ws.jaxrs.nature";
 	public static final String CDI_NATURE="org.jboss.tools.cdi.core.cdinature";
+	public static final String JBOSS6_AS_HOME=System.getProperty("jbosstools.test.jboss.home.6.0");
 	
 	private SWTBotExt botExt = new SWTBotExt();
 	private SWTUtilExt botUtil = new SWTUtilExt(botExt);
@@ -63,33 +64,73 @@ public class Configurators {
 		WorkspaceHelpers.cleanWorkspace();
 	}
 
+	//TODO https://issues.jboss.org/browse/JBIDE-10468
 	@Test
 	public void testJSFConfigurator() throws Exception{
-		createMavenizedDynamicWebProject(PROJECT_NAME_JSF);
+		createMavenizedDynamicWebProject(PROJECT_NAME_JSF, false);
 		addDependencies(PROJECT_NAME_JSF, "com.sun.faces", "mojarra-jsf-api", "2.0.0-b04");
-		assertTrue("Project "+PROJECT_NAME_JSF+" doesn't have "+JSF_NATURE+" nature.",Utils.hasNature(PROJECT_NAME_JSF, JSF_NATURE));
+		assertTrue("Project "+PROJECT_NAME_JSF+" doesn't have "+JSF_NATURE+" nature",Utils.hasNature(PROJECT_NAME_JSF, JSF_NATURE));
+		clean();
+		createMavenizedDynamicWebProject(PROJECT_NAME_JSF, false);
+		addFacesConf(PROJECT_NAME_JSF);
+		assertTrue("Project "+PROJECT_NAME_JSF+" doesn't have "+JSF_NATURE+" nature",Utils.hasNature(PROJECT_NAME_JSF, JSF_NATURE));
+		clean();
+		createMavenizedDynamicWebProject(PROJECT_NAME_JSF, false);
+		addServlet(PROJECT_NAME_JSF,"Faces Servlet","javax.faces.webapp.FacesServlet","1");
+		assertTrue("Project "+PROJECT_NAME_JSF+" doesn't have "+JSF_NATURE+" nature",Utils.hasNature(PROJECT_NAME_JSF, JSF_NATURE));
+		IMavenProjectFacade facade = MavenPlugin.getMavenProjectRegistry().getMavenProject("org.jboss.tools", PROJECT_NAME_JSF,"0.0.1-SNAPSHOT");
+		assertTrue("Project "+PROJECT_NAME_JSF+" doesn't have faces-config.xml file",facade.getProject().getFile("faces-config.xml") != null);
+		clean();
+		createMavenizedDynamicWebProject(PROJECT_NAME_JSF, true);
+		assertTrue("Project "+PROJECT_NAME_JSF+" doesn't have "+JSF_NATURE+" nature",Utils.hasNature(PROJECT_NAME_JSF, JSF_NATURE));
+		
+		
+		
 	}
 	
 	
 	@Test
 	public void testCDIConfigurator() throws Exception{
-		createMavenizedDynamicWebProject(PROJECT_NAME_CDI);
+		createMavenizedDynamicWebProject(PROJECT_NAME_CDI, false);
 		addDependencies(PROJECT_NAME_CDI, "javax.enterprise", "cdi-api","1.1.EDR1.2");
 		assertTrue("Project "+PROJECT_NAME_CDI+" doesn't have "+CDI_NATURE+" nature.",Utils.hasNature(PROJECT_NAME_CDI, CDI_NATURE));
-		createMavenizedEJBProject(PROJECT_NAME_CDI_EJB);
+		createMavenizedEJBProject(PROJECT_NAME_CDI_EJB, false);
 		addDependencies(PROJECT_NAME_CDI_EJB, "javax.enterprise", "cdi-api","1.1.EDR1.2");
 		assertTrue("Project "+PROJECT_NAME_CDI_EJB+" doesn't have "+CDI_NATURE+" nature.",Utils.hasNature(PROJECT_NAME_CDI_EJB, CDI_NATURE));
+		clean();
+		createMavenizedDynamicWebProject(PROJECT_NAME_CDI, true);
+		assertTrue("Project "+PROJECT_NAME_CDI+" doesn't have "+CDI_NATURE+" nature.",Utils.hasNature(PROJECT_NAME_CDI, CDI_NATURE));
+		createMavenizedEJBProject(PROJECT_NAME_CDI_EJB, true);
+		assertTrue("Project "+PROJECT_NAME_CDI_EJB+" doesn't have "+CDI_NATURE+" nature.",Utils.hasNature(PROJECT_NAME_CDI_EJB, CDI_NATURE));
 	}
-	/*
+
 	//@Test
 	public void testJAXRSConfigurator() throws Exception {
-		assertTrue("Project "+PROJECT_NAME+" doesn't have "+JAXRS_NATURE+" nature.",Utils.hasNature(PROJECT_NAME, JAXRS_NATURE));
+		createMavenizedDynamicWebProject(PROJECT_NAME_JSF, false);
+		addDependencies(PROJECT_NAME_JSF, "com.cedarsoft.rest", "jersey", "1.0.0");
+		assertTrue("Project "+PROJECT_NAME_JSF+" doesn't have "+JAXRS_NATURE+" nature.",Utils.hasNature(PROJECT_NAME_JSF, JAXRS_NATURE));
+		clean();
+		createMavenizedDynamicWebProject(PROJECT_NAME_JSF, false);
+		addDependencies(PROJECT_NAME_JSF, "org.jboss.jbossas", "jboss-as-resteasy", "6.1.0.Final");
+		assertTrue("Project "+PROJECT_NAME_JSF+" doesn't have "+JAXRS_NATURE+" nature.",Utils.hasNature(PROJECT_NAME_JSF, JAXRS_NATURE));
+		clean();
+		createMavenizedDynamicWebProject(PROJECT_NAME_JSF, true);
+		assertTrue("Project "+PROJECT_NAME_JSF+" doesn't have "+JAXRS_NATURE+" nature.",Utils.hasNature(PROJECT_NAME_JSF, JAXRS_NATURE));
 	}
-	*/
 	
-	private void createMavenizedDynamicWebProject(String projectName) throws Exception{
+	
+	private void createMavenizedDynamicWebProject(String projectName, boolean runtime) throws Exception{
 		botExt.menu("File").menu("New").menu("Dynamic Web Project").click();
 		botExt.textWithLabel("Project name:").setText(projectName);
+		if(runtime){
+			botExt.button("New Runtime...").click();
+			Thread.sleep(500);
+			botExt.tree().expandNode("JBoss Community").select("JBoss 6.x Runtime");
+			botExt.button("Next >").click();
+			botExt.textWithLabel("Home Directory").setText(JBOSS6_AS_HOME);
+		} else {
+			botExt.comboBoxInGroup("Target runtime").setSelection("<None>");
+		}
 		botExt.button("Next >").click();
 		botExt.button("Next >").click();
 		botExt.checkBox("Generate web.xml deployment descriptor").select();
@@ -108,16 +149,25 @@ public class Configurators {
 	    botExt.button("OK").click();
 	    botUtil.waitForAll();
 		assertTrue("Web project doesn't have maven nature",Utils.isMavenProject(projectName));
-		removeFacets(projectName);
 		updateConf(projectName);
 		assertFalse("Project "+projectName+" have "+JSF_NATURE+" nature.",Utils.hasNature(projectName, JSF_NATURE));
 		assertFalse("Project "+projectName+" have "+JAXRS_NATURE+" nature.",Utils.hasNature(projectName, JAXRS_NATURE));
 		assertFalse("Project "+projectName+" have "+CDI_NATURE+" nature.",Utils.hasNature(projectName, CDI_NATURE));
 	}
 	
-	private void createMavenizedEJBProject(String projectName)throws Exception{
+	private void createMavenizedEJBProject(String projectName, boolean runtime)throws Exception{
 		botExt.menu("File").menu("New").menu("EJB Project").click();
 		botExt.textWithLabel("Project name:").setText(projectName);
+		if(runtime){
+			botExt.button("New Runtime...").click();
+			Thread.sleep(500);
+			botExt.tree().expandNode("JBoss Community").select("JBoss 6.x Runtime");
+			botExt.button("Next >").click();
+			botExt.textWithLabel("Home Directory").setText(JBOSS6_AS_HOME);
+			botExt.button("Finish").click();
+		} else {
+			botExt.comboBoxInGroup("Target runtime").setSelection("<None>");
+		}
 		botExt.button("Finish").click();
 		botUtil.waitForAll(Long.MAX_VALUE);
 		Utils.waitForIdle();
@@ -133,7 +183,6 @@ public class Configurators {
 	    botExt.button("OK").click();
 	    botUtil.waitForAll();
 		assertTrue("EJB project doesn't have maven nature",Utils.isMavenProject(projectName));
-		removeFacets(projectName);
 		updateConf(projectName);
 		assertFalse("Project "+projectName+" have "+JSF_NATURE+" nature.",Utils.hasNature(projectName, JSF_NATURE));
 		assertFalse("Project "+projectName+" have "+JAXRS_NATURE+" nature.",Utils.hasNature(projectName, JAXRS_NATURE));
@@ -201,4 +250,43 @@ public class Configurators {
 		botUtil.waitForAll();
 	}
 	
+	private void addFacesConf(String projectName){
+		SWTBotTree innerBot = botExt.viewByTitle("Package Explorer").bot().tree().select(projectName);
+		ContextMenuHelper.clickContextMenu(innerBot, "New","Other...");
+		botExt.tree().expandNode("JBoss Tools Web").expandNode("JSF").select("Faces Config");
+		botExt.button("Next >").click();
+		botExt.button("Finish").click();
+		updateConf(projectName);
+	}
+	
+	private void addServlet(String projectName, String servletName, String servletClass, String load) throws Exception{
+		IMavenProjectFacade facade = MavenPlugin.getMavenProjectRegistry().getMavenProject("org.jboss.tools", projectName,"0.0.1-SNAPSHOT");
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	    DocumentBuilder docBuilder = factory.newDocumentBuilder();
+	    Document docPom = docBuilder.parse(facade.getProject().getFile("pom.xml").getContents());
+	    Element servletElement = docPom.createElement("servlet");
+	    Element servletNameElement = docPom.createElement("servlet-name");  
+	    Element servletClassElement = docPom.createElement("servlet-class");	    
+	    Element loadElement = docPom.createElement("load-on-startup");
+	    
+	    servletNameElement.setTextContent(servletName);
+	    servletClassElement.setTextContent(servletClass);
+	    loadElement.setTextContent(load);
+	    
+	    Element root = docPom.getDocumentElement();
+	    servletElement.appendChild(servletNameElement);
+	    servletElement.appendChild(servletClassElement);
+	    servletElement.appendChild(loadElement);
+	    root.appendChild(servletElement);
+	    TransformerFactory transfac = TransformerFactory.newInstance();
+		Transformer trans = transfac.newTransformer();
+		StringWriter xmlAsWriter = new StringWriter(); 
+		StreamResult result = new StreamResult(xmlAsWriter);
+		DOMSource source = new DOMSource(docPom);
+		trans.transform(source, result);
+		facade.getProject().getFile("pom.xml").setContents(new ByteArrayInputStream(xmlAsWriter.toString().getBytes("UTF-8")), 0, null);
+		botUtil.waitForAll();
+		updateConf(projectName);	
+	}
 }
