2f802753f7cd405b7843f83259d391cd1b74466b
Denis Golovin <dgolovin@exadel.com>
2012-04-03 21:28:39 +0000

    https://issues.jboss.org/browse/JBIDE-11244 Auto-discover all the modules and add them to the configuration on GWT project import check for none java project added to avoid NP Exceptions
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@40018 a97e2381-89e5-4abb-bab3-167db6db766c

--- maven/plugins/org.jboss.tools.maven.gwt/src/org/jboss/tools/maven/gwt/GWTProjectConfigurator.java
+++ maven/plugins/org.jboss.tools.maven.gwt/src/org/jboss/tools/maven/gwt/GWTProjectConfigurator.java
@@ -17,6 +17,7 @@ import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.MojoExecution;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.m2e.core.project.MavenProjectChangedEvent;
@@ -44,17 +45,23 @@ public class GWTProjectConfigurator extends AbstractProjectConfigurator {
 		boolean configureGWT = store.getBoolean(Activator.CONFIGURE_GWT);
 		log.debug("GWT Entry Point Modules configuration is {}",configureGWT ? "enabled" : "disabled");
 		if(configureGWT && projectConfig.getMavenProject().getPlugin(GWT_WAR_MAVEN_PLUGIN_KEY)!=null) {
-			log.debug("Configure Entry Point Modules for GWT Project {}", projectConfig.getProject().getName());
-			IModule[] modules = ModuleUtils.findAllModules(JavaCore.create(projectConfig.getProject()),false);
-			List<String> modNames = new ArrayList<String>();
-			for (IModule iModule : modules) {
-				modNames.add(iModule.getQualifiedName());
-				log.debug("\t {}",iModule.getQualifiedName());
-			}
-			try {
-				GWTProjectProperties.setEntryPointModules(projectConfig.getProject(), modNames);
-			} catch (BackingStoreException e) {
-				log.error("Exception in Maven GWT Configurator", e);
+			String projectName = projectConfig.getProject().getName();
+			IJavaProject javaProject = JavaCore.create(projectConfig.getProject());
+			if(javaProject!=null) {
+				log.debug("Configure Entry Point Modules for GWT Project {}", projectName);
+				IModule[] modules = ModuleUtils.findAllModules(javaProject,false);
+				List<String> modNames = new ArrayList<String>();
+				for (IModule iModule : modules) {
+					modNames.add(iModule.getQualifiedName());
+					log.debug("\t{}",iModule.getQualifiedName());
+				}
+				try {
+					GWTProjectProperties.setEntryPointModules(projectConfig.getProject(), modNames);
+				} catch (BackingStoreException e) {
+					log.error("Exception in Maven GWT Configurator", e);
+				}
+			} else {
+				log.debug("Skip configurator for non Java project {}",projectName);
 			}
 		}
 	}
