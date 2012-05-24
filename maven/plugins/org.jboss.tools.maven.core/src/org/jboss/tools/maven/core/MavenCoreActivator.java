12eace1851c32efdd8f7946a9a4b790c5aba6454
Fred Bricon <fbricon@gmail.com>
2012-05-24 16:13:32 +0000

    Set maven-compiler-plugin version to 2.4
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41358 a97e2381-89e5-4abb-bab3-167db6db766c

--- maven/plugins/org.jboss.tools.maven.core/src/org/jboss/tools/maven/core/MavenCoreActivator.java
+++ maven/plugins/org.jboss.tools.maven.core/src/org/jboss/tools/maven/core/MavenCoreActivator.java
@@ -716,7 +716,7 @@ public class MavenCoreActivator extends Plugin {
 		org.apache.maven.model.Plugin plugin = new org.apache.maven.model.Plugin();
 		plugin.setGroupId("org.apache.maven.plugins"); //$NON-NLS-1$
 		plugin.setArtifactId("maven-compiler-plugin"); //$NON-NLS-1$
-		plugin.setVersion("2.3.2");
+		plugin.setVersion("2.4");
 		Xpp3Dom configuration = new Xpp3Dom( "configuration" ); //$NON-NLS-1$
 		Xpp3Dom source = new Xpp3Dom("source"); //$NON-NLS-1$
 		source.setValue(compilerLevel); //$NON-NLS-1$
