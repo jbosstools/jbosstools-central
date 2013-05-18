b47784318aa26cb129028a7c96956114abee69db
Snjezana Peco <snjezana.peco@redhat.com>
2013-05-18 16:17:26 +0200

    JBIDE-14332-open cheatsheet on project import

--- examples/plugins/org.jboss.tools.project.examples.cheatsheet/src/org/jboss/tools/project/examples/cheatsheet/Activator.java
+++ examples/plugins/org.jboss.tools.project.examples.cheatsheet/src/org/jboss/tools/project/examples/cheatsheet/Activator.java
@@ -10,9 +10,26 @@
  ************************************************************************************/
 package org.jboss.tools.project.examples.cheatsheet;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.content.IContentDescription;
+import org.eclipse.core.runtime.content.IContentType;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.jboss.tools.project.examples.ProjectExamplesActivator;
+import org.jboss.tools.project.examples.cheatsheet.internal.util.CheatSheetUtil;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -20,11 +37,78 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
 
+	private static final String DOT_CHEATSHEET_XML = ".cheatsheet.xml"; //$NON-NLS-1$
+
+	private static final String CHEATSHEET_XML = "cheatsheet.xml"; //$NON-NLS-1$
+
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.jboss.tools.project.examples.cheatsheet"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
+	
+	IResourceChangeListener listener = new IResourceChangeListener() {
+		
+		public void resourceChanged(IResourceChangeEvent event) {
+			if (event.getType() != IResourceChangeEvent.POST_CHANGE) {
+				return;
+			}
+			IResourceDelta delta = event.getDelta();
+			if (delta == null) {
+				return;
+			}
+			String value = ProjectExamplesActivator.getDefault().getShowCheatsheets();
+			if (ProjectExamplesActivator.SHOW_CHEATSHEETS_NEVER.equals(value)) {
+				return;
+			}
+			final List<IFile> cheatsheets = new ArrayList<IFile>();
+			try {
+				delta.accept(new IResourceDeltaVisitor() {
+				    public boolean visit(IResourceDelta delta) throws CoreException {
+				    	if (delta == null) {
+				    		return false;
+				    	}
+				    	IResource resource = delta.getResource();
+				    	if (resource instanceof IWorkspaceRoot) {
+				    		return true;
+				    	}
+						if (resource instanceof IProject) {
+							if (((IProject) resource).isOpen()) {
+								return true;
+							}
+						}
+				        if (resource instanceof IFile && delta.getKind() == IResourceDelta.ADDED) {
+				        	if (isCheatcheet((IFile) resource)) {
+				        		cheatsheets.add((IFile) resource);
+				        		return false;
+				        	}
+				        }
+
+				        return false;
+				    }
+				});
+			} catch (CoreException e) {
+				Activator.log(e);
+			}
+			if (cheatsheets.size() > 0) {
+				CheatSheetUtil.showCheatsheet(cheatsheets);
+			}
+		}
+
+		public boolean isCheatcheet(IFile file) {
+			boolean candidate = CHEATSHEET_XML.equals(file.getName()) || DOT_CHEATSHEET_XML.equals(file.getName());
+			if (candidate) {
+				try {
+					IContentDescription contentDescription = file.getContentDescription();
+					IContentType contentType = contentDescription.getContentType();
+					return (contentType != null && "org.eclipse.pde.simpleCheatSheet".equals(contentType.getId())); //$NON-NLS-1$
+				} catch (CoreException e) {
+					// ignore
+				}
+			}
+			return false;
+		}
+	};
 	
 	/**
 	 * The constructor
@@ -39,6 +123,7 @@ public class Activator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(listener, IResourceChangeEvent.POST_CHANGE);
 	}
 
 	/*
@@ -47,6 +132,7 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
+		ResourcesPlugin.getWorkspace().removeResourceChangeListener(listener);
 		super.stop(context);
 	}
 
