29b35c18eba184b3336c1e8a01b5845094eabdde
Fred Bricon <fbricon@gmail.com>
2013-05-22 23:21:59 +0200

    JBIDE-14589 : fix FixClasspathConfigurator
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

---
+++ maven/tests/org.jboss.tools.maven.configurators.tests/src/org/jboss/tools/maven/configurators/tests/FixClasspathConfiguratorTest.java
@@ -0,0 +1,52 @@
+/*************************************************************************************
+ * Copyright (c) 2012 Red Hat, Inc. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     JBoss by Red Hat - Initial implementation.
+ ************************************************************************************/
+package org.jboss.tools.maven.configurators.tests;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceDescription;
+import org.eclipse.m2e.core.project.ResolverConfiguration;
+import org.junit.Test;
+
+@SuppressWarnings("restriction")
+public class FixClasspathConfiguratorTest extends AbstractMavenConfiguratorTest {
+	@Test
+	public void testJBIDE14589_duplicateCPE()  throws Exception {
+		
+	    IWorkspaceDescription description = workspace.getDescription();
+	    try {
+	    	description.setAutoBuilding(true);
+		
+			IProject[] projects = importProjects("projects/seam/JBIDE-14589/parent-reactor", 
+					new String[]{ "pom.xml",
+								  "child-module/pom.xml",
+								  "other-module/pom.xml",
+								  }, 
+					new ResolverConfiguration());
+			waitForJobsToComplete();
+			
+			IProject parent = projects[0];
+			IProject child = projects[1];
+			IProject other = projects[2];
+			assertNoErrors(parent);
+			assertNoErrors(child);
+			assertNoErrors(other);
+			
+			updateProject(child);
+			assertNoErrors(child);
+			
+			updateProject(other);
+			assertNoErrors(other);
+		
+	    } finally {
+	    	description.setAutoBuilding(false);
+	    }
+	}
+}
