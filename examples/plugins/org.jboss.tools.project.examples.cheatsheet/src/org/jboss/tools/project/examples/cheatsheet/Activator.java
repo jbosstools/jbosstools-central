c6f1d77bfe5552ac2e2686b33c630ffaea36e54e
Fred Bricon <fbricon@gmail.com>
2013-05-23 00:43:32 +0200

    JBIDE-14332 : only open cheatsheets during project import
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- examples/plugins/org.jboss.tools.project.examples.cheatsheet/src/org/jboss/tools/project/examples/cheatsheet/Activator.java
+++ examples/plugins/org.jboss.tools.project.examples.cheatsheet/src/org/jboss/tools/project/examples/cheatsheet/Activator.java
@@ -62,6 +62,8 @@ public class Activator extends AbstractUIPlugin {
 				return;
 			}
 			final List<IFile> cheatsheets = new ArrayList<IFile>();
+			final Boolean[] createdProject = new Boolean[1];
+			createdProject[0] = false;
 			try {
 				delta.accept(new IResourceDeltaVisitor() {
 				    public boolean visit(IResourceDelta delta) throws CoreException {
@@ -83,6 +85,17 @@ public class Activator extends AbstractUIPlugin {
 				        		return false;
 				        	}
 				        }
+				        if (resource instanceof IFile && delta.getKind() == IResourceDelta.ADDED) {
+				        	if (isCheatcheet((IFile) resource)) {
+				        		cheatsheets.add((IFile) resource);
+				        		//if we know we created (imported) the project, we can stop scanning
+				        		return createdProject[0]?false:true;
+				        	} else //check we're in the middle of an import 
+				        		if (((IFile) resource).equals(resource.getProject().getFile(".project"))) { //$NON-NLS-1$
+ 				        		  createdProject[0] = true;
+				        		  return true;
+				        	}
+				        }
 
 				        return false;
 				    }
@@ -90,13 +103,14 @@ public class Activator extends AbstractUIPlugin {
 			} catch (CoreException e) {
 				Activator.log(e);
 			}
-			if (cheatsheets.size() > 0) {
+			if (createdProject[0] && cheatsheets.size() > 0) {
 				CheatSheetUtil.showCheatsheet(cheatsheets);
 			}
 		}
 
 		public boolean isCheatcheet(IFile file) {
-			boolean candidate = CHEATSHEET_XML.equals(file.getName()) || DOT_CHEATSHEET_XML.equals(file.getName());
+			String fileName = file.getName().toLowerCase();
+			boolean candidate = CHEATSHEET_XML.equals(fileName) || DOT_CHEATSHEET_XML.equals(fileName);
 			if (candidate) {
 				try {
 					IContentDescription contentDescription = file.getContentDescription();
