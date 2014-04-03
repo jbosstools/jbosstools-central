e667fea2ea602b669680dba8a4e27a499edc0fbe
Fred Bricon <fbricon@gmail.com>
2014-04-03 18:15:53 +0200

    JBIDE-16997 : point 'favorite this' button at http://marketplace.eclipse.org/content/jboss-tools-luna
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- central/plugins/org.jboss.tools.central/src/org/jboss/tools/central/actions/FavoriteAtEclipseMarketplaceHandler.java
+++ central/plugins/org.jboss.tools.central/src/org/jboss/tools/central/actions/FavoriteAtEclipseMarketplaceHandler.java
@@ -1,5 +1,5 @@
 /*************************************************************************************
- * Copyright (c) 2008-2011 Red Hat, Inc. and others.
+ * Copyright (c) 2008-2014 Red Hat, Inc. and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,7 +18,8 @@ package org.jboss.tools.central.actions;
  */
 public class FavoriteAtEclipseMarketplaceHandler extends OpenWithBrowserHandler {
 
-	private static final String MARKETPLACE_ECLIPSE_URL = "http://marketplace.eclipse.org/node/420896";
+	//TODO : get this url from upcoming properties provider 
+	private static final String MARKETPLACE_ECLIPSE_URL = "http://marketplace.eclipse.org/content/jboss-tools-luna";
 
 	@Override
 	public String getLocation() {
