9fe83595f5558bf938389b2a3ee642b7acbbfd6d
Mickael Istria <mistria@redhat.com>
2014-06-03 18:53:39 +0200

    JBIDE-17496 JBIDE-17504: Prevent from parallel loading of same repo
    This is a workaround to avoid p2 bug 436378

--- central/plugins/org.jboss.tools.central/src/org/jboss/tools/central/Messages.java
+++ central/plugins/org.jboss.tools.central/src/org/jboss/tools/central/Messages.java
@@ -24,6 +24,7 @@ public class Messages extends NLS {
 	public static String DiscoveryViewer_noFeatureToShow;
 	public static String DiscoveryViewer_clearFilterText;
 	public static String DiscoveryViewer_disableFilters;
+	public static String DiscoveryViewer_waitingForDiscoveryCompletion;
 
 	public static String DiscoveryViewer_FilterSelectionDialog_label;
 	public static String DiscoveryViewer_FilterSelectionDialog_title;
