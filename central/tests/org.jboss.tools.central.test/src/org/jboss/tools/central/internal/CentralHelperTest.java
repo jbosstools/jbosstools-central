b601004a92100bd367aff153a9f6ac16590d51b3
Fred Bricon <fbricon@gmail.com>
2015-06-03 15:12:53 -0400

    Fix compilation error
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- central/tests/org.jboss.tools.central.test/src/org/jboss/tools/central/internal/CentralHelperTest.java
+++ central/tests/org.jboss.tools.central.test/src/org/jboss/tools/central/internal/CentralHelperTest.java
@@ -1,6 +1,7 @@
 package org.jboss.tools.central.internal;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -73,27 +74,6 @@ public class CentralHelperTest {
 			
 		    assertEquals(resolvedUrl, resolvedUrl2);
 			
-		} finally {
-			System.clearProperty(CentralHelper.JBOSS_CENTRAL_WEBPAGE_URL_KEY);
-		}
-	}
-
-	@Test
-	public void testGetCentralFallbackSyspropUrl() throws Exception {
-		String url;
-		try {
-			url = "/foo/bar/central";
-			System.setProperty(CentralHelper.JBOSS_CENTRAL_WEBPAGE_URL_KEY, url);
-			assertEquals(url+"/legacy.html", CentralHelper.getCentralFallbackUrl(null));
-
-			url = "file:///foo/bar/central/index.html";
-			System.setProperty(CentralHelper.JBOSS_CENTRAL_WEBPAGE_URL_KEY, url);
-			assertEquals("file:///foo/bar/central/legacy.html", CentralHelper.getCentralFallbackUrl(null));
-
-		
-			url = "http://central.jboss.org/";
-			System.setProperty(CentralHelper.JBOSS_CENTRAL_WEBPAGE_URL_KEY, url);
-			assertEquals(url+"legacy.html", CentralHelper.getCentralFallbackUrl(null));
 		} finally {
 			System.clearProperty(CentralHelper.JBOSS_CENTRAL_WEBPAGE_URL_KEY);
 		}
