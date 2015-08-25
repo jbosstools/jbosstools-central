aea5a0c69d39fb20e6407a23654c1fbbaa4f3b19
Ilya Buziuk <ibuziuk@exadel.com>
2015-08-25 19:06:13 +0300

    JBIDE-20471: Adding scrollbars to JBoss Central Error composite

--- central/plugins/org.jboss.tools.central/src/org/jboss/tools/central/internal/browser/CentralBrowserErrorWrapper.java
+++ central/plugins/org.jboss.tools.central/src/org/jboss/tools/central/internal/browser/CentralBrowserErrorWrapper.java
@@ -15,6 +15,7 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.SWTError;
 import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
@@ -29,24 +30,33 @@ import org.jboss.tools.central.Messages;
 import org.jboss.tools.usage.event.UsageEventType;
 import org.jboss.tools.usage.event.UsageReporter;
 
+@SuppressWarnings("restriction")
 public class CentralBrowserErrorWrapper {
 
 	/**
-	 * Logs given {@code throwable} (may be wrapped) and shows error message in 
+	 * Logs given {@code throwable} (may be wrapped) and shows error message in
 	 * the {@code parent} composite.
 	 */
 	public void showError(Composite parent, Throwable originalThrowable) {
-		//report that browser is not available
+		// report that browser is not available
 		UsageEventType eventType = JBossCentralActivator.getDefault().getUsedBrowserEventType();
 		UsageReporter.getInstance().trackEvent(eventType.event("N/A")); //$NON-NLS-1$
-		
+
 		Throwable throwable = wrapError(originalThrowable);
 		String errorMessage = throwable.getMessage();
-		
+
 		parent.setLayout(new GridLayout());
-		Composite statusComposite = new Composite(parent, SWT.NONE);
-		Color bgColor= parent.getDisplay().getSystemColor(SWT.COLOR_LIST_BACKGROUND);
-		Color fgColor= parent.getDisplay().getSystemColor(SWT.COLOR_LIST_FOREGROUND);
+
+		// Configure scrolled composite
+		ScrolledComposite scrolledComposite = new ScrolledComposite(parent, SWT.V_SCROLL);
+		scrolledComposite.setLayout(new GridLayout());
+		scrolledComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		scrolledComposite.setExpandVertical(true);
+		scrolledComposite.setExpandHorizontal(true);
+
+		Composite statusComposite = new Composite(scrolledComposite, SWT.NONE);
+		Color bgColor = parent.getDisplay().getSystemColor(SWT.COLOR_LIST_BACKGROUND);
+		Color fgColor = parent.getDisplay().getSystemColor(SWT.COLOR_LIST_FOREGROUND);
 		parent.setBackground(bgColor);
 		parent.setForeground(fgColor);
 		GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, true);
@@ -56,11 +66,14 @@ public class CentralBrowserErrorWrapper {
 
 		IStatus displayStatus = new Status(IStatus.ERROR, JBossCentralActivator.PLUGIN_ID, errorMessage, throwable);
 		new StatusPart(statusComposite, displayStatus);
-		
+
+		scrolledComposite.setMinSize(statusComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		scrolledComposite.setContent(statusComposite);
+
 		final Link link = new Link(parent, SWT.WRAP);
 		link.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END));
 		link.setBackground(bgColor);
-		link.setText("<a>"+Messages.visualEditorFaq+"</a>");//$NON-NLS-1$ $NON-NLS-2$
+		link.setText("<a>" + Messages.visualEditorFaq + "</a>");//$NON-NLS-1$ $NON-NLS-2$
 		link.setToolTipText(Messages.visualEditorFaqLink);
 		link.setForeground(link.getDisplay().getSystemColor(SWT.COLOR_BLUE));
 
@@ -69,26 +82,29 @@ public class CentralBrowserErrorWrapper {
 			public void widgetSelected(SelectionEvent e) {
 				BusyIndicator.showWhile(link.getDisplay(), new Runnable() {
 					public void run() {
-						JBossCentralActivator.openUrl(Messages.visualEditorFaqLink, Display.getDefault().getActiveShell(), true);
+						JBossCentralActivator.openUrl(Messages.visualEditorFaqLink,
+								Display.getDefault().getActiveShell(), true);
 					}
 				});
 			}
 		});
-
 		parent.getParent().layout(true, true);
 	}
-	
+
 	private static Throwable wrapError(Throwable originalThrowable) {
 		if (originalThrowable instanceof SWTError && originalThrowable.getMessage() != null) {
-			String message = originalThrowable.getMessage(); 
+			String message = originalThrowable.getMessage();
 			if (message.contains("No more handles")) {//$NON-NLS-1$
-				/*running under GTK3 and no webkit installed
-				or Xulrunner disbaled by -Dorg.jboss.tools.vpe.loadxulrunner=false flag and no webkit installed
-				this error can be only under Linux. On Windows and OSX default browser always can be created.*/
+				/*
+				 * running under GTK3 and no webkit installed or Xulrunner
+				 * disbaled by -Dorg.jboss.tools.vpe.loadxulrunner=false flag
+				 * and no webkit installed this error can be only under Linux.
+				 * On Windows and OSX default browser always can be created.
+				 */
 				return new Exception(Messages.noEngineError_message, originalThrowable);
 			}
 		}
 		return originalThrowable;
 	}
-	
+
 }
