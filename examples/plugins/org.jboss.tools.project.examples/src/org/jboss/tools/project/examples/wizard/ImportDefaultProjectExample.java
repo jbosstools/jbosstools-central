88e5681700ceeb366820d67d71bac2dbcad3ea72
Jeff MAURY <jmaury@redhat.com>
2017-12-19 12:49:08 +0100

    [JBIDE-25427] - Oxygen.2 TP
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- examples/plugins/org.jboss.tools.project.examples/src/org/jboss/tools/project/examples/wizard/ImportDefaultProjectExample.java
+++ examples/plugins/org.jboss.tools.project.examples/src/org/jboss/tools/project/examples/wizard/ImportDefaultProjectExample.java
@@ -229,7 +229,7 @@ public class ImportDefaultProjectExample extends
 				// copied from JavaProject.buildStructure(...)
 				OpenableElementInfo info = (OpenableElementInfo) object;
 				IClasspathEntry[] resolvedClasspath = ((JavaProject) javaProject).getResolvedClasspath();
-				IPackageFragmentRoot[] children = ((JavaProject) javaProject).computePackageFragmentRoots(resolvedClasspath,false, null /* no reverse map */);
+				IPackageFragmentRoot[] children = ((JavaProject) javaProject).computePackageFragmentRoots(resolvedClasspath,false, false, null /* no reverse map */);
 				info.setChildren(children);
 				((JavaProject) javaProject).getPerProjectInfo().rememberExternalLibTimestamps();
 			}
