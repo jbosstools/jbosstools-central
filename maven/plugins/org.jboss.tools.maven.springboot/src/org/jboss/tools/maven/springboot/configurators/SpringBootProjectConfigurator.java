7f26319653e3d702e2ed403e303bc30e704f3196
Andre Dietisheim <adietish@redhat.com>
2018-02-08 21:49:01 +0100

    [JBIDE-25504] adding utility facet to project dependencies
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- maven/plugins/org.jboss.tools.maven.springboot/src/org/jboss/tools/maven/springboot/configurators/SpringBootProjectConfigurator.java
+++ maven/plugins/org.jboss.tools.maven.springboot/src/org/jboss/tools/maven/springboot/configurators/SpringBootProjectConfigurator.java
@@ -11,12 +11,17 @@
 package org.jboss.tools.maven.springboot.configurators;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.project.MavenProject;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jst.j2ee.internal.project.facet.UtilityFacetInstallDataModelProvider;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
@@ -24,6 +29,8 @@ import org.eclipse.m2e.core.project.MavenProjectChangedEvent;
 import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
 import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
 import org.eclipse.m2e.wtp.WTPProjectsUtil;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.wst.common.componentcore.ModuleCoreNature;
 import org.eclipse.wst.common.componentcore.internal.util.IModuleConstants;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
 import org.eclipse.wst.common.project.facet.core.IFacetedProject;
@@ -31,47 +38,91 @@ import org.eclipse.wst.common.project.facet.core.IFacetedProject.Action;
 import org.eclipse.wst.common.project.facet.core.IProjectFacet;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
 import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
+import org.jboss.tools.foundation.core.plugin.log.StatusFactory;
+import org.jboss.tools.maven.springboot.MavenSpringBootActivator;
 import org.jboss.tools.maven.ui.Activator;
 
 /**
  * 
- * Spring Boot projects configurator.
+ * Spring Boot projects configurator. Adds a java and utility facet to a spring
+ * boot project and it's dependencies.
  *
+ * @author Jeff Maury
+ * @contributor Andre Dietisheim
  */
 public class SpringBootProjectConfigurator extends AbstractProjectConfigurator {
 
-	protected static final IProjectFacet jarUtilityFacet;
-	
+	private static final String PACKAGING_JAR = "jar";
 	private static final String ACTIVATION_PROPERTY = "m2e.springboot.activation";
-
-	static {
-		jarUtilityFacet = ProjectFacetsManager.getProjectFacet(IModuleConstants.JST_UTILITY_MODULE);
-	}
+	private static final IProjectFacet utilityFacet = 
+			ProjectFacetsManager.getProjectFacet(IModuleConstants.JST_UTILITY_MODULE);
 
 	@Override
 	public void configure(ProjectConfigurationRequest request, IProgressMonitor monitor) throws CoreException {
 		MavenProject mavenProject = request.getMavenProject();
 		IProject project = request.getProject();
-		configureInternal(mavenProject, project, monitor);
+		configureProject(mavenProject, project, monitor);
 	}
 
-	private void configureInternal(MavenProject mavenProject, IProject project, IProgressMonitor monitor)
-			throws CoreException {
-
-		if (!isSpringBootConfigurable(mavenProject)) {
-			return;
+	@Override
+	public void mavenProjectChanged(MavenProjectChangedEvent event, IProgressMonitor monitor) throws CoreException {
+		IMavenProjectFacade facade = event.getMavenProject();
+		if (facade != null) {
+			MavenProject mavenProject = facade.getMavenProject(monitor);
+			IProject project = facade.getProject();
+			configureDepdendencies(mavenProject, project, monitor);
 		}
-		final IFacetedProject fproj = ProjectFacetsManager.create(project, true, null);
+		super.mavenProjectChanged(event, monitor);
+	}
+
+	private void configureProject(MavenProject mavenProject, IProject project, IProgressMonitor monitor)
+			throws CoreException {
+		if (isSpringBootConfigurable(mavenProject)) {
+			installUtilityFacet(mavenProject, project, monitor);
+		}
+	}
+
+	private void configureDepdendencies(MavenProject mavenProject, IProject project, IProgressMonitor monitor)
+			throws CoreException {
+		if (isSpringBootConfigurable(mavenProject)) {
+			installUtilityFacet(mavenProject.getName(), getWorkspaceDependencies(mavenProject, project), monitor);
+		}
+	}
+
+	private void installUtilityFacet(String dependentProject, List<IMavenProjectFacade> facades, 
+			IProgressMonitor monitor) throws CoreException {
+		MultiStatus status = new MultiStatus(MavenSpringBootActivator.PLUGIN_ID, 0,
+				NLS.bind("Could not add utility facet to dependencies of project {0}", dependentProject), null);
+		facades.forEach(facade -> {
+			MavenProject mavenDependency = null;
+			try {
+				mavenDependency = facade.getMavenProject(monitor);
+				installUtilityFacet(mavenDependency, facade.getProject(), monitor);
+			} catch (CoreException e) {
+				status.add(StatusFactory.errorStatus(MavenSpringBootActivator.PLUGIN_ID,
+								NLS.bind("Could not add utility facet to project {0}", mavenDependency != null ? 
+										mavenDependency.getName() : facade.getProject().getName()), e));
+			}
+		});
+		if (!status.isOK()) {
+			throw new CoreException(status);
+		}
+	}
+
+	private void installUtilityFacet(MavenProject mavenProject, IProject project, IProgressMonitor monitor)
+			throws CoreException {
+		final IFacetedProject facetedProj = ProjectFacetsManager.create(project, true, null);
 		String packaging = mavenProject.getPackaging();
-		if ("jar".equals(packaging) && (fproj != null) && !fproj.hasProjectFacet(jarUtilityFacet)) {
-			installJarUtilityFacet(fproj, monitor);
+		if (PACKAGING_JAR.equals(packaging)
+				&& facetedProj != null 
+				&& !facetedProj.hasProjectFacet(utilityFacet)) {
+			installUtilityFacet(facetedProj, monitor);
 		}
 	}
 
 	private boolean isSpringBootConfigurable(MavenProject mavenProject) {
 		String springBootActivation = mavenProject.getProperties().getProperty(ACTIVATION_PROPERTY);
-
-		boolean configureSpringBoot;
+		boolean configureSpringBoot = false;
 		if (springBootActivation == null) {
 			IPreferenceStore store = Activator.getDefault().getPreferenceStore();
 			configureSpringBoot = store.getBoolean(Activator.CONFIGURE_SPRING_BOOT);
@@ -81,28 +132,48 @@ public class SpringBootProjectConfigurator extends AbstractProjectConfigurator {
 		return configureSpringBoot;
 	}
 
-	@Override
-	public void mavenProjectChanged(MavenProjectChangedEvent event, IProgressMonitor monitor) throws CoreException {
-		IMavenProjectFacade facade = event.getMavenProject();
-		if (event.getFlags() == MavenProjectChangedEvent.FLAG_DEPENDENCIES && facade != null) {
-			IProject project = facade.getProject();
-			MavenProject mavenProject = facade.getMavenProject(monitor);
-			configureInternal(mavenProject, project, monitor);
+	private void installUtilityFacet(IFacetedProject facetedProj, IProgressMonitor monitor) throws CoreException {
+		if (facetedProj.hasProjectFacet(utilityFacet)) {
+			return;
 		}
-		super.mavenProjectChanged(event, monitor);
+		IProjectFacetVersion facetVersion = utilityFacet.getDefaultVersion();
+		if (facetVersion == null) { // $NON-NLS-1$
+			return;
+		}
+		Set<Action> actions = new HashSet<>();
+		WTPProjectsUtil.installJavaFacet(actions, facetedProj.getProject(), facetedProj);
+		ModuleCoreNature.addModuleCoreNatureIfNecessary(facetedProj.getProject(), monitor);
+		addUtilityFacetAction(actions, facetVersion);
+		facetedProj.modify(actions, monitor);
 	}
 
-	private void installJarUtilityFacet(IFacetedProject fproj, IProgressMonitor monitor) throws CoreException {
-		if (!fproj.hasProjectFacet(jarUtilityFacet)) {
-			IProjectFacetVersion facetVersion = jarUtilityFacet.getDefaultVersion();
-			if (facetVersion != null) { // $NON-NLS-1$
-				Set<Action> actions = new HashSet<>();
-				WTPProjectsUtil.installJavaFacet(actions, fproj.getProject(), fproj);
-				IDataModel model = (IDataModel) new UtilityFacetInstallDataModelProvider().create();
-				actions.add(new Action(Action.Type.INSTALL, facetVersion, model));
-				fproj.modify(actions, monitor);
-			}
-		}
+	private void addUtilityFacetAction(Set<Action> actions, IProjectFacetVersion facetVersion) {
+		IDataModel model = (IDataModel) new UtilityFacetInstallDataModelProvider().create();
+		actions.add(new Action(Action.Type.INSTALL, facetVersion, model));
 	}
 
+	protected List<IMavenProjectFacade> getWorkspaceDependencies(MavenProject mavenProject, IProject project) {
+		return mavenProject.getArtifacts().stream()
+			.filter(artifact -> isDeployableScope(artifact.getScope()))
+			.map(artifact -> {
+				IMavenProjectFacade dependency = 
+						projectManager.getMavenProject(artifact.getGroupId(), artifact.getArtifactId(), artifact.getVersion());
+				if (dependency != null
+					&& !dependency.getProject().equals(project) 
+					&& dependency.getFullPath(artifact.getFile()) != null) {
+					return dependency;
+				} else {
+					return null;
+				}
+			})
+			.filter(Objects::nonNull)
+			.distinct()
+			.collect(Collectors.toList());
+	}
+
+	private boolean isDeployableScope(String scope) {
+		return Artifact.SCOPE_COMPILE.equals(scope)
+				//MNGECLIPSE-1578 Runtime dependencies should be deployed 
+				|| Artifact.SCOPE_RUNTIME.equals(scope);
+	}
 }
