648b0fc0a7b9fd1dc57c49ef802eb4bdf3b84129
Vojtěch Průša <prusa.vojtech@gmail.com>
2018-09-11 16:36:49 +0200

    Added optional artifactId and groupId matcher
    Signed-off-by: Vojtěch Průša <prusa.vojtech@gmail.com>

--- maven/test-framework/org.jboss.tools.maven.reddeer/src/org/jboss/tools/maven/reddeer/wizards/MavenProjectWizardSecondPage.java
+++ maven/test-framework/org.jboss.tools.maven.reddeer/src/org/jboss/tools/maven/reddeer/wizards/MavenProjectWizardSecondPage.java
@@ -11,27 +11,38 @@
 package org.jboss.tools.maven.reddeer.wizards;
 
 import org.eclipse.reddeer.jface.wizard.WizardPage;
+import org.eclipse.reddeer.swt.api.TableItem;
 import org.eclipse.reddeer.swt.condition.TableHasRows;
 import org.eclipse.reddeer.swt.impl.combo.DefaultCombo;
 import org.eclipse.reddeer.swt.impl.table.DefaultTable;
 import org.eclipse.reddeer.workbench.core.condition.JobIsRunning;
+import org.jboss.tools.maven.reddeer.wizards.matchers.ArtifactIdAndGroupIdMatcher;
 import org.eclipse.reddeer.common.wait.TimePeriod;
 import org.eclipse.reddeer.common.wait.WaitUntil;
 import org.eclipse.reddeer.common.wait.WaitWhile;
 import org.eclipse.reddeer.core.reference.ReferencedComposite;
+
+import java.util.List;
 
 public class MavenProjectWizardSecondPage extends WizardPage{
 	
 	public MavenProjectWizardSecondPage(ReferencedComposite referencedComposite) {
 		super(referencedComposite);
 	}
+	
+	public void selectArchetype(String catalog, final String archetype){
+		selectArchetype(catalog, archetype, null);
+	}
 
-	public void selectArchetype(String catalog, String archetype){
+	public void selectArchetype(String catalog, final String archetype, final String groupId){
 		new DefaultCombo(0).setSelection(catalog);
 		new WaitWhile(new JobIsRunning(), TimePeriod.VERY_LONG);
 		new WaitUntil(new TableHasRows(new DefaultTable()),TimePeriod.LONG);
-		// TODO: RedDeer has to be enhanced to find item by text of other rows then just first one
-		new DefaultTable().getItem(archetype,1).select();
+		DefaultTable table = new DefaultTable();
+				 	 	    
+	    @SuppressWarnings("unchecked")
+		List<TableItem> items = table.getItems(new ArtifactIdAndGroupIdMatcher(groupId, archetype));
+	    items.get(0).select();
 	}
 
 }
