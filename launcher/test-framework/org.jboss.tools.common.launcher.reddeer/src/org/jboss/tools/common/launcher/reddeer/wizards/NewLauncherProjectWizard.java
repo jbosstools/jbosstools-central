b58130287d54f03db997addaba72d17602832d9d
Zbynek Cervinka <zcervink@redhat.com>
2018-12-10 15:30:15 +0100

    JBIDE-26520 Add the test-framework maven plugin and update pom and manifest to 3.10.2.
    Signed-off-by: Zbynek Cervinka <zcervink@redhat.com>

---
+++ launcher/test-framework/org.jboss.tools.common.launcher.reddeer/src/org/jboss/tools/common/launcher/reddeer/wizards/NewLauncherProjectWizard.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.common.launcher.reddeer.wizards;
+
+
+import org.eclipse.reddeer.jface.wizard.WizardDialog;
+import org.eclipse.reddeer.common.wait.TimePeriod;
+import org.eclipse.reddeer.common.wait.WaitUntil;
+import org.eclipse.reddeer.common.wait.WaitWhile;
+import org.eclipse.reddeer.swt.api.Button;
+import org.eclipse.reddeer.swt.impl.browser.InternalBrowser;
+import org.eclipse.reddeer.swt.impl.button.NextButton;
+import org.eclipse.reddeer.swt.impl.button.PushButton;
+import org.eclipse.reddeer.swt.impl.shell.DefaultShell;
+import org.eclipse.reddeer.swt.impl.tree.DefaultTreeItem;
+import org.eclipse.reddeer.workbench.core.condition.JobIsRunning;
+import org.eclipse.reddeer.workbench.impl.shell.WorkbenchShell;
+import org.jboss.tools.central.reddeer.api.JavaScriptHelper;
+import org.jboss.tools.central.reddeer.wait.CentralIsLoaded;
+import org.eclipse.reddeer.eclipse.ui.dialogs.NewWizard;
+
+
+/**
+ * 
+ * @author zcervink
+ *
+ */
+public class NewLauncherProjectWizard extends WizardDialog {
+	
+	/**
+	 * Opens new application wizard via shell menu File - New.
+	 */
+	public void openWizardFromShellMenu() {
+		new WorkbenchShell().setFocus();
+		new NewWizard().open();
+		new DefaultShell("New").setFocus(); 
+		new DefaultTreeItem("Launcher", "Launcher project").select(); 
+		new NextButton().click();
+		new DefaultShell("New Launcher project");
+	}
+	
+	/**
+	 * Opens new application wizard via link in the Red Hat Central
+	 */
+	public void openWizardFromCentral() {
+		new WaitUntil(new CentralIsLoaded(),TimePeriod.LONG);
+		JavaScriptHelper.getInstance().setBrowser(new InternalBrowser());
+		JavaScriptHelper.getInstance().clickWizard("Launcher Application");
+		new DefaultShell("New Launcher project");
+	}
+	
+	/**
+	 * Finishes this wizard
+	 */
+	@Override
+	public void finish() {		
+		finish(TimePeriod.getCustom(1800));
+	}
+	
+	/**
+	 * Finishes this wizard
+	 */
+	@Override
+	public void finish(TimePeriod period) {	
+		Button button = new PushButton("Finish");
+		button.click();
+		new WaitWhile(new JobIsRunning(), period);
+	}
+	
+	/**
+	 * Returns if the "Finish" button is enabled
+	 * @return
+	 */
+	@Override
+	public boolean isFinishEnabled() {
+		PushButton finishButton = new PushButton("Finish");
+		return (finishButton.isEnabled());
+	}
+}
